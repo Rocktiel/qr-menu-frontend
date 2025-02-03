f40be7367609c91adc549e9a5153ac4163872044
Rocktiel <93122097+Rocktiel@users.noreply.github.com>
2025-02-03 05:37:16 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
   //   locales: ["tr", "en"],
   //   defaultLocale: "tr",
   // },
-  
+  env: {
   NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
     NEXT_PUBLIC_CLIENT_URL:
