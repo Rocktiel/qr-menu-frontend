8606a0f1b4d827a58917044d5862624827984013
Rocktiel <93122097+Rocktiel@users.noreply.github.com>
2025-02-03 03:05:08 +0300

    Update page.js

--- src/app/menu/[restaurantId]/[tableNumber]/page.js
+++ src/app/menu/[restaurantId]/[tableNumber]/page.js
@@ -1,87 +1,22 @@
-"use client"; // Bu satır önemli
+import MenuPage from "./MenuPage";
 
-
-
-import React, { useState, useEffect } from "react";
-
-import CustomerMenuClient from "./CustomerMenuClient";
-
-
-
-export default function CustomerMenu({ params }) {
-
-  const resolvedParams = React.use(params); // params'ı React.use() ile çöz
-
-  const [restaurantId, setRestaurantId] = useState(null);
-
-  const [tableNumber, setTableNumber] = useState(null);
-
-
-
-  useEffect(() => {
-
-    if (resolvedParams) {
-
-      const { restaurantId, tableNumber } = resolvedParams;
-
-      setRestaurantId(restaurantId);
-
-      setTableNumber(tableNumber);
-
-    }
-
-  }, [resolvedParams]);
-
-
-
-  if (!restaurantId || !tableNumber) {
-
-    return <div>Loading...</div>;
-
-  }
-
-
-
-  return (
-
-    <CustomerMenuClient restaurantId={restaurantId} tableNumber={tableNumber} />
-
-  );
-
+export default function Page({ params }) {
+  return <MenuPage params={params} />;
 }
 
-
-
 export async function generateStaticParams() {
-
-  // Örnek statik parametreler
-
   return [
-
     {
-
-      restaurantId: '1',
-
-      tableNumber: '1'
-
+      restaurantId: "1",
+      tableNumber: "1",
     },
-
     {
-
-      restaurantId: '1',
-
-      tableNumber: '2'
-
+      restaurantId: "1",
+      tableNumber: "2",
     },
-
     {
-
-      restaurantId: '2',
-
-      tableNumber: '1'
-
-    }
-
+      restaurantId: "2",
+      tableNumber: "1",
+    },
   ];
-
 }
